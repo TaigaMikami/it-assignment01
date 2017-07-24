ee0d3499d564f0eff43f97aeb5083b1aa4ed8011
ahaha0807 <b5331@oic.jp>
2017-07-24 14:18:01 +0900

    Review
    前コミットと分けた意味なかったです。
    主なレビューはこっちでやってます

--- vending_machine.rb
+++ vending_machine.rb
@@ -23,12 +23,13 @@ end
 class VendingMachine
   AVAILABLE_MONEY = [10,50,100,500,1000]
 
-  attr_accessor :total, :sale_amount # procies
+  attr_accessor :total, :sale_amount # -> [typo] sale_proceeds
   def initialize
     @total = 0
-    @drink_menu = {} #飲み物のハッシュ配列
+    @drink_menu = {} #飲み物のハッシュ配列 -> [Bad]ハッシュ配列…？一瞬理解を悩ませるので、「ハッシュの配列」と書いたほうが良いです 
     @sale_proceeds = 0
-    5.times{ store Drink.coke}
+    5.times{ store Drink.coke}  # [Good!]5回登録するというのが理解しやすい！
+                                # [Hmm...] できることなら ↑ の5は変数にしてしまって、変数を書き換えれば商品数が管理できるようにする形にしたほうが良い 
   end
 
   def insert(money)#お金投入
@@ -38,31 +39,34 @@ class VendingMachine
       puts "払い戻し:#{money}円"
       return money
     end
-    puts @total
+    puts @total # [Bad]何が起きているのかわかりにくいので、「#{@total} 円投入されています」とかのほうが良さそう
   end
 
   def store(drink)#飲み物格納
     nil.tap do
-      if !(@drink_menu.has_key?(drink.name)) #drink.nameの飲み物がない時格納
+      if !(@drink_menu.has_key?(drink.name)) #drink.nameの飲み物がない時格納 -> [Question] unless は使わないのですか？ 
         @drink_menu[drink.name] = { price: drink.price, drinks: [] }
       end
       #名前があれば在庫追加
       @drink_menu[drink.name][:drinks].push(drink)
     end
+
+    # [Suggestion]登録本数を引数で受け取れるようにして、回数分登録できるようにすれば拡張しやすいかも
   end
 
-  def stock_info#飲み物格納情報
+  def stock_info#飲み物格納情報 -> [Hmm...] 細かいですが、 display_stock とかにすれば、なんのためのメソッド化理解しやすいと思います
     puts @drink_menu.map{|name, info|  [name, { price: info[:price], stock: info[:drinks].size }]}
   end
 
-  def available_puchase(drink_name)#購入可能確認 # purchase
+  # [Suggiestion] purchase でだけ呼び出すことが可能になるように private メソッドにしてしまってもいいのでは？
+  def available_puchase(drink_name)#購入可能確認 -> [typo] purchase 
     can_buy = @drink_menu.select{|_, info| total >= info[:price] && info[:drinks].any?}.keys
-    puts "購入可能#{can_buy}"
-    puts can_buy.include?(drink_name)
+    puts "購入可能#{can_buy}" # [Good!] この書き方だと可読性が高い！
+    puts can_buy.include?(drink_name) # [Question] これはデバッグ用に表示していたものですか？
     return can_buy.include?(drink_name)
   end
 
-  def purchase(drink_name)
+  def purchase(drink_name) # [Good!] メソッドの処理が見通しやすいです！
     if available_puchase(drink_name)
       drink = @drink_menu[drink_name][:drinks].pop
       @sale_proceeds += drink.price
@@ -74,30 +78,19 @@ class VendingMachine
 end
 
 
-#インスタンス作成 <- コードを読めばわかるので、コメント内容は「処理開始」とかのほうが良さそうです。 @ahaha0807
+#インスタンス作成 <- コードを読めばわかるので、コメント内容は「自販機の作成」とかのほうが良さそうです。 
 machine = VendingMachine.new
 
-money = gets.to_i # [Bad]いきなり入力待ちにすると、どうすればいいのかわからなくなるので、インフォメーションを表示する @ahaha0807
-machine.insert(money) # [Good!]動作対象.処理内容（）という形でメソッド名等を付けてる @ahaha0807
+money = gets.to_i # [Bad]いきなり入力待ちにすると、どうすればいいのかわからなくなるので、インフォメーションを表示する 
+machine.insert(money) # [Good!]動作対象.処理内容（）という形でメソッド名等を付けてる 
 money = gets.to_i 
 machine.insert(money)
 
-# [Soso]初期化のタイミングは78行目のタイミングのほうがわかりやすい @ahaha0807
+# [Hmm...] 新しいドリンクの登録は自販機の初期化のタイミングのほうがわかりやすいです。 
 machine.store(Drink.water)
 machine.store(Drink.redbull)
 
 machine.stock_info
-machine.available_puchase(:coke) # [Soso] これは結果を true/false を返すようにした方が理解しやすいかも？ @ahaha0807
-# [Bad] available_purchase だと日本語では「購入可能な購入」とかになってしまって、意味が通じにくいので、 check_purchaseとかにするとわかりやすそう @ahaha0807
-machine.purchase(:coke)
-
-=begin
-[Soso]
-
-if machine.check_purchase
-  machine.purchase
-end
-
-とかにすれば、処理の流れが見通しやすいと思います
-@ahaha0807
-=end
+machine.available_puchase(:coke)  # [Hmm...] これは結果を true/false を返すようにした方が理解しやすいかも？ 
+                                  # [Bad] available_purchase だと日本語では「購入可能な購入」とかになってしまって、意味が通じにくいので、 check_purchaseとかにするとわかりやすそう 
+machine.purchase(:coke) # [Bad] available_purchase は purchase の中で読んでいるので、改めて呼ぶ必要はないと思います
